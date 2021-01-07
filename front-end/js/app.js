61a36eda3fa2abfe04f87f172ba5f486e3aa7828
chris434 <christopherpearce589@gmail.com>
2021-01-07 13:42:20 +0000

    description formatted

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -19,10 +19,13 @@ const createTemplate = async(res) => {
 const productTemplate = (data) => {
     let { name, price, description, imageUrl } = data
     return `<div class="item-holder"><a href="#"><img class="item-holder-img" src="${imageUrl}"></a><h2><a href="#">${name}</a></h2>
-        <span>${formattedPrice(price)}</span><p>${description}</p></div>
+        <span>${formattedPrice(price)}</span><p>${formattedDescription(description)}</p></div>
         `
 }
 const formattedPrice = (price) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price)
+}
+const formattedDescription = (des) => {
+    return des.substr(0, 40).padEnd(43, '.')
 }
 createTemplate(getAll())
