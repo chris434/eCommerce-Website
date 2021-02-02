7b75655e19f8e1232d829e40be47097fa758ba7d
chris434 <christopherpearce589@gmail.com>
2021-02-02 20:42:38 +0000

    new price formatted

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,6 +1,7 @@
 //formats the price into dollars
 const formattedPrice = (price) => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price)
+
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price / 100)
 }
 const optionsTemplate = (arry, selected) => {
     const options = (opt) => {
